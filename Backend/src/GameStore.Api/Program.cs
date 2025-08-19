985949290c0b168236d4e09cfe44d366c47e8dc0
yoonsang Lee <ysle@pm.me>
2025-08-20 08:39:46 +0900

    chore: add sample in-memory data model

--- Backend/src/GameStore.Api/Program.cs
+++ Backend/src/GameStore.Api/Program.cs
@@ -3,6 +3,30 @@ using GameStore.Api.Models;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<Game> games =
+[
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "Street Fighter 11",
+        Genre = "Action",
+        Price = 19.99m,
+        ReleaseDate = new DateOnly(1992, 7, 15)
+    },
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "Final Fantasy XIV",
+        Genre = "Roleplaying",
+        Price = 59.99m,
+        ReleaseDate = new DateOnly(2010, 9, 30)
+    },
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "FIFA 23",
+        Genre = "Sports",
+        Price = 69.99m,
+        ReleaseDate = new DateOnly(2022, 9, 27)
+    }
+];
 
 app.MapGet("/", () => "Welcome to the Game Store API!");
 
