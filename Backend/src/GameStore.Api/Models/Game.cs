32f680c049a4bc9ca5c45889983d4e0779ddf735
yoonsang Lee <ysle@pm.me>
2025-08-20 08:36:58 +0900

    feat(model): add GameModel

---
+++ Backend/src/GameStore.Api/Models/Game.cs
@@ -0,0 +1,12 @@
+namespace GameStore.Api.Models;
+
+public class Game
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Genre { get; set; }
+    public decimal Price { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+
+
+}
