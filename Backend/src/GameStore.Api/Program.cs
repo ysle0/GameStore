182755ce74512daba049266c5481a1fb4d18feb1
yoonsang Lee <ysle@pm.me>
2025-08-26 01:03:48 +0900

    fix: Guid parse error

--- Backend/src/GameStore.Api/Program.cs
+++ Backend/src/GameStore.Api/Program.cs
@@ -1,42 +1,60 @@
 using GameStore.Api.Models;
 
-const string GetGameEndPointName = "GetGame";
+const string getGameEndPointName = "GetGame";
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 List<Genre> genres = [
-    new() { Id = Guid.Parse("1ee90aa6-0fb2-4799-87c6-84a0c35d2510"), Name = "Action", },
-    new() { Id = Guid.Parse("9cd66e98-1d83-400f-96a7-fac5bfe0416f"), Name = "Kids And Family", },
-    new() { Id = Guid.Parse("73df4803-f510-4fff-ab75-344ac3b13eaf"), Name = "Racing", },
-    new() { Id = Guid.Parse("58cb3ad0-1fb0-41d6-a02a-fe3a7f9fdac"), Name = "Roleplaying", },
-    new() { Id = Guid.Parse("2854a607-1447-42e6-b21a-ff6432d83b1"), Name = "Sports", },
+    new() {
+        Id = new Guid("1EE90AA6-0FB2-4799-87C6-84A0C35D2510"),
+        Name = "Action",
+    },
+    new() {
+        Id = new Guid("9cd66e98-1d83-400f-96a7-fac5bfe0416f"),
+        Name = "Kids And Family",
+    },
+    new() {
+        Id = new Guid("73df4803-f510-4fff-ab75-344ac3b13eaf"),
+        Name = "Racing",
+    },
+    new() {
+        Id = new Guid("58cb3ad0-1fb0-41d6-a02a-fe3a7f9fdacb"),
+        Name = "Roleplaying",
+    },
+    new() {
+        Id = new Guid("2b54a607-1a47-42e6-b21a-ff6432d83b14"),
+        Name = "Sports",
+    },
 ];
 
 List<Game> games = [
     new Game {
         Id = Guid.NewGuid(),
-        Name = "Street Fighter 11",
+        Name = "Street Fighter II",
         Genre = genres[0],
         Price = 19.99m,
         ReleaseDate = new DateOnly(1992, 7, 15),
-        Description = "",
+        Description =
+            "Street Fighter II, the most popular fighting game of all time, is back! With a new look and a new fighting style, Street Fighter 11 is the ultimate fighting game for all ages.",
     },
     new Game {
         Id = Guid.NewGuid(),
         Name = "Final Fantasy XIV",
-        Genre = genres[1],
+        Genre = genres[3],
         Price = 59.99m,
         ReleaseDate = new DateOnly(2010, 9, 30),
-        Description = "",
+        Description =
+            "Final Fantasy XIV is a 2022 action role-playing game developed by Square Enix and published by Square Enix Co., Ltd, and This game broadens the user's experience to infinity!",
     },
     new Game {
         Id = Guid.NewGuid(),
         Name = "FIFA 23",
-        Genre = genres[2],
+        Genre = genres[^1],
         Price = 69.99m,
         ReleaseDate = new DateOnly(2022, 9, 27),
-        Description = "",
+        Description =
+            "FIFA 23 is a 2022 sports video game developed and published by EA Sports. It is the third installment in the FIFA series, following FIFA 22 and FIFA 21, be the team and take a win!",
     }
 ];
 
@@ -53,13 +71,13 @@ app.MapGet("/games/{id}", (Guid id) => {
 
         return Results.Ok(game);
     })
-    .WithName(GetGameEndPointName);
+    .WithName(getGameEndPointName);
 
 app.MapPost("/games", (Game game) => {
     game.Id = Guid.NewGuid();
     games.Add(game);
     return Results.CreatedAtRoute(
-        GetGameEndPointName,
+        getGameEndPointName,
         new { id = game.Id },
         game);
 }).WithParameterValidation();
