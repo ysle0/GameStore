1e76d81e5e2a1b9fe8e1805d8cbb3eb0c05f5db7
yoonsang Lee <ysle@pm.me>
2025-08-28 00:54:46 +0900

    chore: update deleteGame using batch delete

--- Backend/src/GameStore.Api/Features/Games/DeleteGame/DeleteGameEndpoint.cs
+++ Backend/src/GameStore.Api/Features/Games/DeleteGame/DeleteGameEndpoint.cs
@@ -1,11 +1,15 @@
 using GameStore.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Features.Games.DeleteGame;
 
 public static class DeleteGameEndpoint {
     public static void MapDeleteGame(this IEndpointRouteBuilder app) {
-        app.MapDelete("/{id:guid}", (Guid id, GameStoreData data) => {
-            data.RemoveGame(id);
+        app.MapDelete("/{id:guid}", (Guid id, GameStoreContext dbCtx) => {
+            dbCtx.Games
+                .Where(g => g.Id == id)
+                .ExecuteDelete();
+            
             return Results.NoContent();
         });
     }
