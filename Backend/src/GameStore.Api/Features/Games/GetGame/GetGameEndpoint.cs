6aabd0e3863cf5c66d00288db790c8823725ed6c
yoonsang Lee <ysle@pm.me>
2025-08-28 08:34:52 +0900

    chore: update getGame with Task to not block exectuion.

--- Backend/src/GameStore.Api/Features/Games/GetGame/GetGameEndpoint.cs
+++ Backend/src/GameStore.Api/Features/Games/GetGame/GetGameEndpoint.cs
@@ -7,13 +7,18 @@ namespace GameStore.Api.Features.Games.GetGame;
 public static class GetGameEndpoint {
     public static void MapGetGame(this IEndpointRouteBuilder app) {
         app.MapGet("/{id:guid}", (Guid id, AppContext dbCtx) => {
-            Game? foundGame = dbCtx.Games.Find(id);
-            if (foundGame is null) {
-                return Results.NotFound();
-            }
+            Task<Game?> foundGameTask = dbCtx.Games.FindAsync(id)
+                .AsTask();
 
-            var dto = GameDetailDto.FromGame(foundGame);
-            return Results.Ok(dto);
+            return foundGameTask.ContinueWith(task => {
+                Game? foundGame = task.Result;
+                if (foundGame is null) {
+                    return Results.NotFound();
+                }
+
+                var dto = GameDetailDto.FromGame(foundGame);
+                return Results.Ok(dto);
+            });
         }).WithName(EndpointNames.GetGame);
     }
 }
