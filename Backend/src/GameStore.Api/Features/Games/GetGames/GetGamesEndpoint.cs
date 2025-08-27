ee4c965b4e892e1a796e254defb316a92838a731
yoonsang Lee <ysle@pm.me>
2025-08-28 00:48:15 +0900

    chore: update get all games

--- Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
+++ Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
@@ -1,18 +1,21 @@
 using GameStore.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Features.Games.GetGames;
 
 public static class GetGamesEndpoint {
     public static void MapGetGames(this IEndpointRouteBuilder app) {
-        app.MapGet("/", (GameStoreData data) => data
-            .GetAllGames()
-            .Select(g => new GetGameDto(
-                g.Id,
-                g.Name,
-                g.Genre!.Name,
-                g.Price,
-                g.ReleaseDate
-            ))
+        app.MapGet("/", (GameStoreContext dbCtx) =>
+            dbCtx.Games
+                .Include(g => g.Genre)
+                .Select(g => new GetGameDto(
+                    g.Id,
+                    g.Name,
+                    g.Genre!.Name,
+                    g.Price,
+                    g.ReleaseDate
+                ))
+                .AsNoTracking()
         );
     }
 }
