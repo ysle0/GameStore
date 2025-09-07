fd81517025aa5ee708116c15985e31212fc3cb81
yoonsang Lee <ysle@pm.me>
2025-09-08 00:24:58 +0900

    chore: add missing ef files

---
+++ Backend/src/GameStore.Api/Data/Migrations/20250907152031_update.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUri",
+                table: "Games",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUri",
+                table: "Games");
+        }
+    }
+}
